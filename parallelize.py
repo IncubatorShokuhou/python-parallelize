f8a47ecf6f72c2bedd0696b7cfb914a8f252d930
Hao Lyu <lh@lasg.iap.ac.cn>
2020-07-07 10:54:40 +0800

    raise error on unrecognized parameter; change cpu_number to n_jobs.

--- parallelize.py
+++ parallelize.py
@@ -3,40 +3,41 @@ from itertools import islice
 import os
 import multiprocessing
 __version__ = '1.0.0.0_qb'
-def get_per_cpu(seq,cpu_number= "half"):
-    if isinstance(cpu_number, int):
-        cpu_number_in = cpu_number
-    elif isinstance(cpu_number,float):
-        cpu_number_in = int(cpu_number)
-    elif isinstance(cpu_number,str):
-        if cpu_number == "all":
-            cpu_number_in = multiprocessing.cpu_count()
-        elif cpu_number == "half":
-            cpu_number_in = int( multiprocessing.cpu_count() / 2 )
-        elif cpu_number == "quarter":
-            cpu_number_in = int( multiprocessing.cpu_count() / 4 )
+def get_per_cpu(seq,n_jobs= "half"):
+    if isinstance(n_jobs, int):
+        n_jobs_in = n_jobs
+    elif isinstance(n_jobs,float):
+        n_jobs_in = int(n_jobs)
+    elif isinstance(n_jobs,str):
+        if n_jobs == "all":
+            n_jobs_in = multiprocessing.cpu_count()
+        elif n_jobs == "half":
+            n_jobs_in = int( multiprocessing.cpu_count() / 2 )
+        elif n_jobs == "quarter":
+            n_jobs_in = int( multiprocessing.cpu_count() / 4 )
         else:
             try:
-                cpu_number_in =int(cpu_number)
+                n_jobs_in =int(n_jobs)
             except:
-                print("error,cannot recognize cpu number")
-    if cpu_number_in > multiprocessing.cpu_count():
-        print("cpu number is too large. Turn into max cpu count.")
-        cpu_number_in1 = min(cpu_number_in,multiprocessing.cpu_count())
-        cpu_number_in = cpu_number_in1
+                print("Error,cannot recognize n_jobs.Please use an integer or 'all','half','quarter' ")
+                raise
+    if n_jobs_in > multiprocessing.cpu_count():
+        print("n_jobs is too large. Turn into max cpu count.")
+        n_jobs_in1 = min(n_jobs_in,multiprocessing.cpu_count())
+        n_jobs_in = cpu_number_in1
     #print("cpu number:",cpu_number_in)
-    def per_cpu(seq,cpu_number_in):
-        return (islice(seq, cpu, None, cpu_number_in) for cpu in range(cpu_number_in))
-    return per_cpu(seq,cpu_number_in)
+    def per_cpu(seq,n_jobs_in):
+        return (islice(seq, cpu, None, n_jobs_in) for cpu in range(n_jobs_in))
+    return per_cpu(seq,n_jobs_in)
 
-def parallelize(seq,cpu_number= "half"):
+def parallelize(seq,n_jobs= "half"):
     """
     input:
     seq: for loop
-    cpu_number: number of cpu to be used. Default value is "half".
+    n_jobs: number of cpu to be used. Default value is "half".
     """
     pids = []
-    fork = get_per_cpu(seq,cpu_number=cpu_number)
+    fork = get_per_cpu(seq,n_jobs=n_jobs)
     for slice in fork:
         pid = os.fork()
         if pid == 0:
@@ -50,7 +51,7 @@ def parallelize(seq,cpu_number= "half"):
         os.waitpid(pid,0)
 
 if __name__ == '__main__':
-    for i in parallelize(range(10),cpu_number = "half"):
+    for i in parallelize(range(10),n_jobs = "half"):
         print(i)
         import time
         time.sleep(2)
